fcce2aea25a00fb18d9422bcb69191a6010596e6
dubeygaurav-Tech <dubeygaurav-Tech@users.noreply.github.com>
2022-12-15 14:13:39 +0100

    updated location

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,16 +37,16 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2663.747305185288!2d11.566239316508224!3d48.11511016070314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479ddf152da11271%3A0xaf06a94dfcb0f99!2sThusneldastra%C3%9Fe%201%2C%2081543%20M%C3%BCnchen!5e0!3m2!1sen!2sde!4v1671108762468!5m2!1sen!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-          </iframe>
-          <div className="bg-gray-900 relative flex flex-wrap py-10 rounded shadow-md">
+            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 ADDRESS
               </h2>
               <p className="mt-1">
-                Thusneldastr. 10<br />
-                Munich 81543
+                97 Warren St. <br />
+                New York, NY 10007
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -54,12 +54,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                dubeygaurav.tech@email.com
+                reedbarger@email.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">+49 17667403973</p>
+              <p className="leading-relaxed">123-456-7890</p>
             </div>
           </div>
         </div>
@@ -72,8 +72,8 @@ export default function Contact() {
             Hire Me
           </h2>
           <p className="leading-relaxed mb-5">
-            Glad you visited the website. Let me know if you would like to connect for opportunities.
-            Drop your contact informations and I will reply to it promptly.
+          Glad you visited the website. Let me know if you would like to connect for opportunities.
+          Drop your contact informations and I will reply to it
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
