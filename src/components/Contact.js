2413945845d49770ddf5fb7e2f99b48dab09963e
dubeygaurav-Tech <dubeygaurav-Tech@users.noreply.github.com>
2022-12-15 21:20:34 +0100

    updated form structure for contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,12 +5,26 @@ export default function Contact() {
   const [email, setEmail] = React.useState("");
   const [message, setMessage] = React.useState("");
 
+  const [formDate,setFormData] = React.useState({
+      name:"",  email:"",message:""
+  })
+
   function encode(data) {
     return Object.keys(data)
       .map(
         (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
       )
       .join("&");
+  }
+
+  function handleChange(e){
+      console.log(e.target.value)
+      setFormData(prevFormData=>{
+        return {
+            ...prevFormData,
+            [e.target.value]:e.target.vale
+        }
+      })
   }
 
   function handleSubmit(e) {
@@ -74,7 +88,8 @@ export default function Contact() {
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+              //onChange={(e) => setName(e.target.value)}
+              onChange={handleChange}
             />
           </div>
           <div className="relative mb-4">
@@ -86,7 +101,8 @@ export default function Contact() {
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              //onChange={(e) => setEmail(e.target.value)}
+              onChange={handleChange}
             />
           </div>
           <div className="relative mb-4">
@@ -99,7 +115,8 @@ export default function Contact() {
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+              //onChange={(e) => setMessage(e.target.value)}
+              onChange={handleChange}
             />
           </div>
           <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
