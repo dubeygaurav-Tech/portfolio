ce28cd9ca065c4c5d75158fb41ad3a3684f89677
dubeygaurav-Tech <dubeygaurav.tech@gmail.com>
2023-01-26 22:13:04 +0100

    added sapient logo

--- src/data.js
+++ src/data.js
@@ -23,7 +23,7 @@ export const projects = [
     subtitle: "React and Firebase",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
+    image: "./sapient.png",
     link: "https://reactbootcamp.com",
   },
   {
