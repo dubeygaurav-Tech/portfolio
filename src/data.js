4963571ba777b3581f5b35d72992d6e8b9c5590c
dubeygaurav-Tech <dubeygaurav-Tech@users.noreply.github.com>
2023-01-26 12:57:50 +0100

    added JPMC logo

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ export const projects = [
     subtitle: "React and Python",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
+    image: "./JPMC.png",
     link: "https://reactbootcamp.com",
   },
   {
